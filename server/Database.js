f893c51da35f8e1c5e9df32ac9ad301ee1e0ec8e
Manish <manishspoojary19@gmail.com>
2023-09-26 19:10:34 +0530

    Update Database.js

--- server/Database.js
+++ server/Database.js
@@ -1,9 +1,10 @@
 const {createPool} = require("mysql");
 const pool=createPool({
-  host:'sql303.infinityfree.com',
-  user:'if0_35034260',
-  password:'q7MxoZdXgCnL',
-  database:'if0_35034260_todo'
+  host: 'sql12.freemysqlhosting.net',
+  user: 'sql12649222',
+  password: 'VvZsJWFIGh',
+  port:'3306',
+  database: 'sql12649222'
 })
 
 
