ccd5d71a9937f01c720601b677b7945a19419629
Manish <manishspoojary19@gmail.com>
2023-09-27 22:28:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://manishpoojary.kesug.com/"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(cookieParser());
 // app.use(
 //   session({
